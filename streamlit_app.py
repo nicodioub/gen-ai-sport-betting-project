3b78ed394c28fe232b42cfd92aae890958085184
Nicolas Dioubaté <128827085+nicodioub@users.noreply.github.com>
2025-02-13 09:49:02 +0000

    new setup

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,27 @@ import streamlit as st
 
 st.title("🎈 My new app")
 st.write(
-    "Hello world !"
+    " **Hello world !**"
 )
+
+
+import streamlit as st
+import pandas as pd
+
+st.write("Hello, what would you like our AI to predict")
+
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+})
+
+option = st.selectbox(
+    "Which number do you like best?",
+    df['first column']
+)
+
+if option > 2:
+    st.write("The option selected is above 2")
+
+else :
+    st.write("The option selected is below or equal to 2")
